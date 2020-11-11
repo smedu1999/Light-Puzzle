bdea97c25160d12f823a576c2c12097a42ed9e8a
Szalekhun <szalek99a@gmail.com>
2020-11-11 11:38:35 +0100

    laser

--- LightPuzzle/Assets/line.cs
+++ LightPuzzle/Assets/line.cs
@@ -8,17 +8,45 @@ public class Line : MonoBehaviour
     public int maxstep = 200;
     private int maxref = 20;
 
-    //private LineRenderer lr;
+    private LineRenderer lr;
+    private Ray ray;
+    private RaycastHit hit;
+    private Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-    //    lr = GetComponent<LineRenderer>();
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ray = new Ray(transform.position, transform.forward);
+        lr.positionCount = 1;
+        lr.SetPosition(0, transform.position);
+        float remainstep = maxstep;
+
+        for(int i = 0; i < maxref; i++)
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, remainstep))
+            {
+
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount - 1, hit.point);
+                remainstep -= Vector3.Distance(ray.origin, hit.point);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                if (hit.collider.gameObject.layer != 8)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount - 1, ray.origin + ray.direction * remainstep);
+            }
+        }
       /*  RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
@@ -30,7 +58,7 @@ public class Line : MonoBehaviour
         else lr.SetPosition(1, new Vector3(0, 0, 5000));*/
     }
 
-    void OnDrawGizmos()
+    /*void OnDrawGizmos()
     {
         DrawLine(this.transform.position, this.transform.forward, maxref);
     }
@@ -50,11 +78,10 @@ public class Line : MonoBehaviour
         {
             pos += dir * maxstep;
         }
-
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(oldpos, pos);
         if (hit.collider.gameObject.layer != 8) return;
         DrawLine(pos, dir, remainingref - 1);
-    }
+    }*/
 }
 
