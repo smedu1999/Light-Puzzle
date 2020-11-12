47f871ce40519e5e05bb298f247cf9e2118263c9
smedu1999 <smodics.roland@gmail.com>
2020-11-12 08:55:50 +0100

    íyx

--- LightPuzzle/Assets/DragObject.cs
+++ LightPuzzle/Assets/DragObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DragObject : MonoBehaviour
 {
-    private Vector3 mOffset;
+    /*private Vector3 mOffset;
     private float mZCoord;
 
     void OnMouseDown()
@@ -17,13 +17,38 @@ public class DragObject : MonoBehaviour
     {
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = mZCoord;
-
+        
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
     void OnMouseDrag()
     {
         transform.position = new Vector3(GetMouseAsWorldPoint().x + mOffset.x, transform.position.y, GetMouseAsWorldPoint().z + mOffset.z);
+        //transform.position = GetMouseAsWorldPoint() + mOffset;
     }
+   
+    */
+    private Vector3 screenPoint;
+    private Vector3 offset;
 
+    void OnMouseDown()
+    {
+        /*Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(mouse);*/
+        
+    }
+    void OnMouseDrag()
+    {
+     
+        Plane plane = new Plane(Vector3.up, new Vector3(0, transform.position.y, 0));
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
+    }
+  
 }
+    
+
