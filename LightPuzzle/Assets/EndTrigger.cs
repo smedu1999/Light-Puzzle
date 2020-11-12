8afe30e5755e72068aeec2a6e9feddd0c776102f
whattila <ahhidati070@gmail.com>
2020-11-12 17:13:13 +0100

    added endTrigger

---
+++ LightPuzzle/Assets/EndTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public UIManager uim;
+    void OnTriggerEnter()
+    {
+        uim.LoadLevel("Congratulations");
+    }
+}
